7d46cd4af141d30b99c05051d041fff93da124d7
Josh Holtz <josh@rokkincat.com>
2017-03-01 10:48:35 -0600

    Initil commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,20 @@
+import Vapor
+import VaporPostgreSQL
+
+let drop = Droplet()
+
+// Adding preparations for models
+drop.preparations += Post.self
+
+// Adding database provider
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
